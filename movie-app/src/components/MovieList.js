26d50b4640518831187c3cddbb3cca6ab3589ee5
Pedro <pedrogdsilva410@gmail.com>
2024-01-24 23:53:34 +0100

    add: overlay on hover

--- movie-app/src/components/MovieList.js
+++ movie-app/src/components/MovieList.js
@@ -3,11 +3,14 @@ import React from "react";
 const MovieList = (props) => {
 	return (
 		<>
-			{props.movies.map((movie, index) => <div className="d-flex justify-content-start m-3">
-				<img src={movie.Poster} alt="movie poster"></img>
-			</div>)}
+			{props.movies.map((movie, index) => (
+				<div className="image-container d-flex justify-content-start m-3">
+					<img src={movie.Poster} alt="movie poster"></img>
+					<div className="overlay d-flex align-item-center justify-content"></div>
+				</div>
+			))}
 		</>
-	)
+	);
 };
 
 export default MovieList;
